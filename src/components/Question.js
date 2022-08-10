a97390468a65569af1f14a204f52881cf31f8a45
Kevin <magavakevin10@gmail.com>
2022-08-10 12:25:40 -0400

    done

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    //create timer const that will run the setTimeout
+    const timer = setTimeout(() => setTimeRemaining(timeRemaining => timeRemaining - 1), 1000);
+
+    //if timeRemaining is 0, the answer is false and we rest the timer
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+      return;
+    }
+
+    //we clear the timer when there are no more questions
+    return function (){
+      clearTimeout(timer);
+    }
+
+    //the useEffect runs whenever timeRemaining changes (countdown) or when the question is answered
+  }, [timeRemaining, onAnswered])
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
